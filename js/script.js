e787f7ddd3a3e55cf2de52371a69631d833aac72
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-09-08 20:13:24 +0100

    update js/script.js EventListener fubction on the filter Button

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const dropdownOptions = document.querySelectorAll('.dropdown-option');
 dropdownOptions.forEach(option => {
     option.addEventListener('click', function() {
         console.log('Filter by:', this.getAttribute('data-value'));
-        document.querySelector('.filter-button').innerHTML = '<i class="fas fa-filter"></i> Filter by ' + this.innerText;
+        document.querySelector('.filter-button').innerHTML = '<i class="fas fa-filter"></i> Filter by ' + ':' + ' ' + this.innerText;
         document.querySelector('.filter-container').classList.remove('show-dropdown');
     });
 });
