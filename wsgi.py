c9ee1a9d86cfce58cc72a8baf8c6a78a1e3c2cb8
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-09-24 23:23:50 +0100

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,8 @@ from app import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    from werkzeug.serving import run_simple
+
+    port = int(environ.get("PORT", 5000))
+    run_simple('0.0.0.0', port, app)
