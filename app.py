726ef93d7fea1381b04e01f7af74d916e10525a5
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-09-25 00:23:27 +0100

    update flask app: remove the app.run from app.py

--- app.py
+++ app.py
@@ -371,6 +371,6 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
