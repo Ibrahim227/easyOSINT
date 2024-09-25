d19f28484aaa0789851e4de04982227bbf08979f
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-09-25 17:06:58 +0100

    update db.py: fixing some internal error

--- api/db.py
+++ api/db.py
@@ -8,7 +8,7 @@ from flask.cli import with_appcontext
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
-            "sqlite_db", detect_types=sqlite3.PARSE_DECLTYPES
+            "../sqlite_db", detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
 
@@ -25,7 +25,7 @@ def close_db(e=None):
 def init_db():
     db = get_db()
 
-    with current_app.open_resource("schema.sql") as f:
+    with current_app.open_resource("../schema.sql") as f:
         db.executescript(f.read().decode("utf8"))
 
 
